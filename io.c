7251395713c65acc04aabb6dc37825a0c1c56250
Maciej Rzasa <maciejrzasa@gmail.com>
2025-02-08 14:09:56 +0100

    fixup! fixup! Fix #20919: clear ungetc buffer on IO#seek with encoding

--- io.c
+++ io.c
@@ -638,6 +638,8 @@ io_unread(rb_io_t *fptr, bool discard_rbuf)
     char *buf;
 
     rb_io_check_closed(fptr);
+    fptr->cbuf.off = 0;
+    fptr->cbuf.len = 0;
     if (fptr->rbuf.len == 0 || fptr->mode & FMODE_DUPLEX) {
         return;
     }
